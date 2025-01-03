dd917828e90e9b8a7b8bbd782c2855dc5ec5252e
Nguyen-Viet-Trung <viettrung0302@gmail.com>
2025-01-03 15:16:33 +0700

    Fix daily schedule bug

--- QuartzSchedule/src/main/java/com/example/QuartzSchedule/config/QuartzSchedulerConfig.java
+++ QuartzSchedule/src/main/java/com/example/QuartzSchedule/config/QuartzSchedulerConfig.java
@@ -3,23 +3,16 @@ package com.example.QuartzSchedule.config;
 import org.quartz.CronScheduleBuilder;
 import org.quartz.JobBuilder;
 import org.quartz.JobDetail;
-import org.quartz.Scheduler;
-import org.quartz.SchedulerException;
 import org.quartz.Trigger;
 import org.quartz.TriggerBuilder;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import com.example.QuartzSchedule.QuartzJob.DailyEmailJob;
 
-import jakarta.annotation.PostConstruct;
-
 @Configuration
 public class QuartzSchedulerConfig {
 
-    @Autowired
-    private Scheduler scheduler;
 
     @Bean
     public JobDetail dailyEmailJobDetail() {
@@ -30,17 +23,13 @@ public class QuartzSchedulerConfig {
     }
 
     @Bean
-    public Trigger dailyEmailJobTrigger() {
+    public Trigger dailyEmailJobTrigger(JobDetail dailyJobDetail) {
         return TriggerBuilder.newTrigger()
-                .forJob(dailyEmailJobDetail())
+                .forJob(dailyJobDetail)
                 .withIdentity("dailyEmailTrigger", "email-triggers")
                 .withSchedule(CronScheduleBuilder.dailyAtHourAndMinute(9, 0)) // Gửi lúc 9h sáng mỗi ngày
                 .build();
     }
 
-    @PostConstruct
-    public void scheduleDailyEmailJob() throws SchedulerException {
-        scheduler.scheduleJob(dailyEmailJobDetail(), dailyEmailJobTrigger());
-    }
 }
 
